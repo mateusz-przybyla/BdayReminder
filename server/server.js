ca1a16fe44e86b8ff41c248bc4a6cf63b8bb8e4c
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2024-10-21 08:42:10 +0200

    Replaced Webpack with Vite

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 import express from "express";
 
 const app = express();
-const port = 4000;
-/*
-app.get("/", (req, res) => {
+const port = 8080;
+
+app.get("/api", (req, res) => {
   console.log("Connected to Node");
-  res.send("Hello world!");
+  //res.send("Hello world!");
+  res.json({ fruits: ["apple", "orange", "banana"] });
 });
-*/
+
 app.post("/post", (req, res) => {
   console.log("Connected to React");
   res.redirect("/");
