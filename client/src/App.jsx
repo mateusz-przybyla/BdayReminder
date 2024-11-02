a495ee40d7e4649d86aa67a3e2a33ff3b57a005c
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2024-11-02 19:02:22 +0100

    Deleted React data grid

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,23 +8,10 @@ import CreateArea from "./components/CreateArea";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-import Box from "@mui/material/Box";
-import { DataGrid, GridToolbarContainer } from "@mui/x-data-grid";
-import AddIcon from "@mui/icons-material/Add";
-import {
-  Button,
-  Stack,
-  MenuItem,
-  FormControl,
-  InputLabel,
-  Select,
-} from "@mui/material";
-
 //import { getList, setItem } from "./services/list";
 
 function App() {
   const [data, setData] = useState([]);
-  const [height, setHeight] = React.useState(400);
 
   useEffect(() => {
     const fetchBirthday = async () => {
@@ -65,36 +52,6 @@ function App() {
     }
   };
 
-  const columns = [
-    { field: "id", headerName: "ID", width: 90 },
-    {
-      field: "firstName",
-      headerName: "First name",
-      width: 150,
-      editable: true,
-    },
-    {
-      field: "lastName",
-      headerName: "Last name",
-      width: 150,
-      editable: true,
-    },
-    {
-      field: "birthdate",
-      headerName: "Birthdate",
-      width: 150,
-      editable: true,
-    },
-    {
-      field: "comment",
-      headerName: "Comment",
-      width: 150,
-      editable: true,
-    },
-  ];
-
-  const rows = data;
-
   return (
     <>
       <Header />
@@ -120,51 +77,6 @@ function App() {
         )}
 
         <CreateArea onAdd={addBirthday} />
-
-        <br />
-        <hr />
-        <br />
-        <h2>Final List:</h2>
-        <br />
-
-        <Stack style={{ width: "100%" }} alignItems="flex-start" spacing={2}>
-          <FormControl fullWidth>
-            <InputLabel
-              htmlFor="height-of-container"
-              id="height-of-container-label"
-            >
-              Height of the container
-            </InputLabel>
-            <Select
-              label="Main Grouping Criteria"
-              onChange={(event) => setHeight(Number(event.target.value))}
-              value={height}
-              id="height-of-container"
-              labelId="height-of-container-label"
-            >
-              <MenuItem value="300">300px</MenuItem>
-              <MenuItem value="400">400px</MenuItem>
-              <MenuItem value="500">500px</MenuItem>
-            </Select>
-          </FormControl>
-          <div style={{ height, width: "100%" }}>
-            <DataGrid
-              editMode="row"
-              rows={rows}
-              columns={columns}
-              initialState={{
-                pagination: {
-                  paginationModel: {
-                    pageSize: 5,
-                  },
-                },
-              }}
-              pageSizeOptions={[5, 10, 25, { value: -1, label: "All" }]}
-              checkboxSelection
-              disableRowSelectionOnClick
-            />
-          </div>
-        </Stack>
 
         <div>
           <br />
