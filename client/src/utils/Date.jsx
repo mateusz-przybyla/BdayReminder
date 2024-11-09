dcdbd309ad30942e93d755b54a4d399f254b20b1
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2024-11-09 16:48:11 +0100

    Changed birthday card color depending on birthdate

---
+++ client/src/utils/Date.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const currentDay = new Date().getDate();
+const currentMonth = new Date().getMonth() + 1;
+const currentYear = new Date().getFullYear();
+
+export { currentDay, currentMonth, currentYear };
