69b1ddeeb9fe5d7dd295396de426f1f9ffd249f0
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2024-12-02 18:47:37 +0100

    Changed EditModal width property (width => maxWidth)

--- client/src/components/EditBdayForm.jsx
+++ client/src/components/EditBdayForm.jsx
@@ -46,7 +46,7 @@ const EditBdayForm = (props) => {
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
-    width: 400,
+    maxWidth: 400,
     bgcolor: "background.paper",
     boxShadow: 24,
     p: 4,
