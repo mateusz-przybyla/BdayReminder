f775190c50fd6d9660a70dc3301c098624b2d2ca
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2024-12-04 13:03:13 +0100

    Get day and month in 2 digit format

--- client/src/utils/date&time.jsx
+++ client/src/utils/date&time.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const currentDay = new Date().getDate();
-const currentMonth = new Date().getMonth() + 1;
+const currentDay = ("0" + new Date().getDate()).slice(-2);
+const currentMonth = ("0" + (new Date().getMonth() + 1)).slice(-2);
 const currentYear = new Date().getFullYear();
 const currentTime = new Date().toLocaleTimeString();
 
